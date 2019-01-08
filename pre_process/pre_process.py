1158b9fd772a18d4e31609e92ad823f567d1855a
lijinchao2007 <lijinchao2007@163.com>
2019-01-08 17:56:03 +0800

    update

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -2,15 +2,21 @@
 import os
 import shutil
 import random
-
+import cv2
 
 def mkdir_if_not_exist(path):
     if not os.path.exists(os.path.join(path)):
         os.makedirs(os.path.join(path))
 
 
-# 大类拆分成小类
+# 写数据 flag:'a+':追加 |'w+'
+def write_data(file_name, data, flag):
+    with open(file_name, flag) as f:
+        f.write(data)
+
+
 def data_pre_process(root_path, old_label_file, output_label_file):
+    class_index = "0"
     files_list = list()
 
     old_label_path = os.path.join(root_path, old_label_file)
@@ -23,7 +29,27 @@ def data_pre_process(root_path, old_label_file, output_label_file):
         label = label.replace('\n', '').rstrip()
         str_list = label.split(' ')
         print str_list
+        image_path = str_list[0]
+        print image_path
+        label_path = image_path.replace('.jpg', '.txt').replace('.png', '.txt')
+        print label_path
 
+        write_data(output_label_path, image_path + '\n', 'a+')      # 写图片路径到output_label_file
+        image = cv2.imread(os.path.join(root_path, image_path))
+        h, w, c = image.shape
+        print('(h, w, c): (%d, %d, %d)' % (h, w, c))
+        save_str = class_index + " "
+        for i, temp in enumerate(str_list[2:]):
+            print i, str
+            if i % 5 == 0:
+                save_str += str(float(temp) / w) + " "
+            elif i % 5 == 1:
+            elif i % 5 == 2:
+            elif i % 5 == 3:
+            elif i % 5 == 4:
+                # 写标签到（output_label_path, label_path）
+                write_data(os.path.join(output_label_path, label_path), save_str + '\n', 'a+')
+                save_str = class_index + " "
     return
 
 
