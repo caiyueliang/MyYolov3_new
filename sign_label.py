08913f34515d6e113103894bc4c8bb79b18d797c
lijinchao2007 <lijinchao2007@163.com>
2019-01-09 14:38:11 +0800

    update

--- sign_label.py
+++ sign_label.py
@@ -3,7 +3,7 @@ import cv2
 import os
 import time
 import shutil
-import common as common
+import subprocess
 
 # ['EVENT_FLAG_ALTKEY', 'EVENT_FLAG_CTRLKEY', 'EVENT_FLAG_LBUTTON', 'EVENT_FLAG_MBUTTON', 'EVENT_FLAG_RBUTTON',
 # 'EVENT_FLAG_SHIFTKEY', 'EVENT_LBUTTONDBLCLK', 'EVENT_LBUTTONDOWN', 'EVENT_LBUTTONUP', 'EVENT_MBUTTONDBLCLK',
@@ -18,11 +18,49 @@ import common as common
 # def mouse_click_events(event, x, y, flags, param):
 #     if event == cv2.EVENT_LBUTTONDBLCLK:
 #         cv2.circle(img, (x, y), 100, (255, 0, 0), -1)
+def exe_cmd(cmd):
+    s = subprocess.Popen(str(cmd), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    s.wait()
+    print(s)
+
+
+def mkdir_if_not_exist(path):
+    if not os.path.exists(os.path.join(path)):
+        os.makedirs(os.path.join(path))
+
+
+def get_files(file_dir):
+    L = []
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            L.append(os.path.join(root, file))      # os.path.join 获取完整路径
+    return L
+
+
+def get_img_files(file_dir):
+    L = []
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jepg"):
+                L.append(os.path.join(root, file))      # os.path.join 获取完整路径
+    return L
+
+
+# 写数据 flag:'a+':追加 |'w+'
+def write_data(file_name, data, flag):
+    with open(file_name, flag) as f:
+        f.write(data)
+
+
+# 读数据 flag:'r'
+def read_data(file_name, flag):
+    with open(file_name, flag) as f:
+        return f.read()
 
 
 class SignCarLabel:
     def __init__(self, root_dir, image_dir, label_file, index_file, copy_dir, copy_file):
-        self.img_files = common.get_files(os.path.join(root_dir, image_dir))
+        self.img_files = get_files(os.path.join(root_dir, image_dir))
         self.image_dir = image_dir
         self.label_file = label_file
         self.car_points = []
@@ -46,14 +84,14 @@ class SignCarLabel:
     def sign_start(self, restart=False):
         times = 1
 
-        common.mkdir_if_not_exist(os.path.join(self.copy_dir, self.image_dir))
+        mkdir_if_not_exist(os.path.join(self.copy_dir, self.image_dir))
 
         cv2.namedWindow('sign_image')
         cv2.setMouseCallback('sign_image', self.mouse_click_events)    # 鼠标事件绑定
 
         if restart is False:
             try:
-                start_i = int(common.read_data(self.index_file, 'r'))
+                start_i = int(read_data(self.index_file, 'r'))
                 print('start_index: ' + str(start_i))
             except Exception, e:
                 print e
@@ -90,9 +128,9 @@ class SignCarLabel:
                     #     data += ' ' + str(x/float(times)) + ' ' + str(y/float(times))
                     data += '\n'
 
-                    common.write_data(self.label_file, data, 'a+')
+                    write_data(self.label_file, data, 'a+')
                     start_i += 1
-                    common.write_data(self.index_file, str(start_i), 'w')
+                    write_data(self.index_file, str(start_i), 'w')
                     self.car_points = []
                     break
 
@@ -108,19 +146,19 @@ class SignCarLabel:
 
                     data += '\n'
 
-                    common.write_data(self.label_file, data, 'a+')
-                    common.write_data(self.copy_file, data, 'a+')
+                    write_data(self.label_file, data, 'a+')
+                    write_data(self.copy_file, data, 'a+')
                     shutil.copy(self.img_files[start_i], os.path.join(self.copy_dir, self.image_dir,
                                                                       self.img_files[start_i].split('/')[-1]))
 
                     start_i += 1
-                    common.write_data(self.index_file, str(start_i), 'w')
+                    write_data(self.index_file, str(start_i), 'w')
                     self.car_points = []
                     break
 
                 if k == ord('d'):
                     print('delete ...')
-                    common.exe_cmd('rm -r ' + self.img_files[start_i])
+                    exe_cmd('rm -r ' + self.img_files[start_i])
                     self.img_files.pop(start_i)
 
                     print('[total] %d; [index] %d; [name] %s' % (len(self.img_files), start_i, self.img_files[start_i]))
@@ -167,25 +205,56 @@ class SignCarLabel:
                     k = cv2.waitKey(1) & 0xFF
                     if k == ord('y'):
                         print('save ...')
-                        common.write_data(os.path.join('.', label_path.split('/')[-1]), line, 'a+')
+                        write_data(os.path.join('.', label_path.split('/')[-1]), line, 'a+')
                         break
                     if k == ord('n'):
                         break
         return
 
 
+def show_image(image_file):
+    label_file = image_file.replace('.jpg', '.txt').replace('.png', '.txt')
+    print(image_file)
+    print(label_file)
+
+    image = cv2.imread(image_file)
+    h, w, c = image.shape
+    print('(h, w, c): (%d, %d, %d)' % (h, w, c))
+
+    with open(label_file, 'r') as file:
+        label_list = file.readlines()
+
+    for label in label_list:
+        temp_list = label.rstrip().split(' ')
+        x1 = float(temp_list[1]) * w - (float(temp_list[3]) * w / 2)
+        y1 = float(temp_list[2]) * h - (float(temp_list[4]) * h / 2)
+
+        x2 = float(temp_list[1]) * w + (float(temp_list[3]) * w / 2)
+        y2 = float(temp_list[2]) * h + (float(temp_list[4]) * h / 2)
+
+        if int(temp_list[0]) == 0:
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        elif int(temp_list[0]) == 1:
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2)
+
+    cv2.imshow('show_image', image)
+    cv2.waitKey(0)
+
+
 if __name__ == '__main__':
-    root_dir = '../Data/car_rough_detect/car_detect_train/'
-    copy_dir = '../Data/car_rough_detect/car_detect_test/'
-    image_dir = "failed_3"
-    label_file = "./label/car_label.txt"
-    copy_file = "./label/copy_label.txt"
-    index_file = "./label/car_index.txt"
+    show_image("../Data/yolo/yolo_data_new/car_detect_train/daozha_1/480466_闽DF3N37.jpg")
 
-    sign_point = SignCarLabel(root_dir=root_dir, image_dir=image_dir, label_file=label_file,
-                              index_file=index_file, copy_dir=copy_dir, copy_file=copy_file)
-
-    sign_point.sign_start()
+    # root_dir = '../Data/yolo/yolo_data_new/car_detect_train/'
+    # copy_dir = '../Data/yolo/yolo_data_new/car_detect_test/'
+    # image_dir = "failed_3"
+    # label_file = "./label/car_label.txt"
+    # copy_file = "./label/copy_label.txt"
+    # index_file = "./label/car_index.txt"
+    #
+    # sign_point = SignCarLabel(root_dir=root_dir, image_dir=image_dir, label_file=label_file,
+    #                           index_file=index_file, copy_dir=copy_dir, copy_file=copy_file)
+    #
+    # sign_point.sign_start()
 
     # root_dir = '../Data/car_rough_detect/car_detect_train/'
     # label_path = '../Data/car_rough_detect/car_detect_train/car_detect_train_label.txt'
