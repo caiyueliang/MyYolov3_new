75910052b8e8b7b31fc2d3ee7b8e7f23acd519a2
caiyueliang <393900414@qq.com>
2019-01-10 22:07:13 +0800

    update

--- sign_label.py
+++ sign_label.py
@@ -415,6 +415,7 @@ if __name__ == '__main__':
     # sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_train/failed_3/", process_all=True)
     # sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_train/ketuo_1/", process_all=True)
     # sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_train/other_1/", process_all=True)
-    sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_train/szlg_1/", process_all=True)
-
+    # sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_train/szlg_1/", process_all=True)
+    sign_label.sign_images(root_path="../Data/yolo/yolo_data_new/car_detect_test/", process_all=True)
+    
     # sign_label.sign_images(root_path="../Data/yolo/yolo_data_new_1/car_detect_train/", process_all=False)
