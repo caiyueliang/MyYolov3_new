1ce9a54a17c413d4c69d54243138709e84407070
caiyueliang <393900414@qq.com>
2019-01-16 20:34:29 +0800

    update

--- detect.py
+++ detect.py
@@ -36,7 +36,7 @@ parser.add_argument('--class_num', type=int, default=2, help='class_num')
 parser.add_argument('--class_path', type=str, default='../Data/yolo/yolo_data_new/lpr.names', help='path to class label file')
 
 parser.add_argument('--conf_thres', type=float, default=0.9, help='object confidence threshold')
-parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
+parser.add_argument('--nms_thres', type=float, default=0.2, help='iou thresshold for non-maximum suppression')
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--n_cpu', type=int, default=0, help='number of cpu threads to use during batch generation')
 parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
